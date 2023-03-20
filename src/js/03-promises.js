c43335902958bc2014cfe85998e3a07a3042fbf3
MateuszBieda <mateusbieda@gmail.com>
2023-03-21 00:42:04 +0100

    "task_3 update "

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const form = document.querySelector(".form");
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) =>{ 
+  setInterval (()=> {
   if (shouldResolve) {
     // Fulfill
+    resolve({position, delay})
   } else {
     // Reject
+    reject({position, delay})
+  }
+}, delay)
+})
+}
+
+function onSubmit (event) {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);  
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let actualDelay = delay + step * i;
+
+  createPromise(position, actualDelay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
   }
 }
+
+
+  form.addEventListener('submit', onSubmit);
